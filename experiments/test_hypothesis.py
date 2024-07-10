4d2376b7084fd6ed108ce172880526f974bd65da
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-07-10 17:08:57 -0400

    Demonstration.

---
+++ experiments/test_hypothesis.py
@@ -0,0 +1,13 @@
+from hypothesis.extra.numpy import arrays
+from hypothesis import given
+import numpy as np
+
+def my_sum(arr):
+    total = 0
+    for value in arr.ravel():
+        total += value
+    return total
+
+@given(arr=arrays(np.uint64, (2, 2)))
+def test_my_sum(arr):
+    assert my_sum(arr) == arr.sum()
