98457016e3bb8f7e60cf1a867e522ccf022a0976
Itamar Turner-Trauring <itamar@pythonspeed.com>
2025-06-20 14:47:12 -0400

    Start on Rust support.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 (
  (nil . ((eval . (setq ispell-personal-dictionary (concat (locate-dominating-file default-directory ".dir-locals.el") "book/.aspell.en.pws")))))
  (markdown-mode . ((eval . (lsp-mode))))
+ (nil . ((eval . (setq lsp-rust-analyzer-linked-projects ["./book/part_2_compilation/frequent_rs/Cargo.toml"]))))
 )
---
+++ book/part_2_compilation/frequent_rs/src/lib.rs
@@ -0,0 +1,61 @@
+//! A Rust extension that has different implementations of finding the most
+//! common value in a Python list of integers.
+
+use pyo3::types::{PyNone, PySequence};
+use pyo3::{BoundObject, prelude::*};
+use std::collections::HashMap;
+
+/// Wrap a Python object so that it can be used in Rust HashMaps.
+struct HashEqWrapper<'a> {
+    pyobject: Bound<'a, PyAny>,
+}
+
+/// Use the Python object's hash to support Rust hashing.
+impl<'a> std::hash::Hash for HashEqWrapper<'a> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        state.write_isize(self.pyobject.hash().unwrap());
+    }
+}
+
+/// Use the Python object's equality to support Rust equality.
+impl<'a> PartialEq for HashEqWrapper<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.pyobject.eq(&other.pyobject).unwrap()
+    }
+}
+
+impl<'a> Eq for HashEqWrapper<'a> {}
+
+/// Given a Python sequence of integers (which must fit in a signed 64-bit
+/// integer), return the most frequent value.
+#[pyfunction]
+fn most_frequent_naive<'py>(values: &'py Bound<'py, PySequence>) -> PyResult<Bound<'py, PyAny>> {
+    let mut counts = HashMap::new();
+    for pyobject in values.try_iter()? {
+        // Deal with case where iteration fails:
+        let pyobject = pyobject?;
+        // Wrap the object in a wrapper that uses Python's hashing and equality
+        // to implement Rust's hashing and equality:
+        let pyobject = HashEqWrapper { pyobject };
+        // If there's an entry, increment it, otherwise insert 1
+        counts
+            .entry(pyobject)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    }
+    // Find the maximum count:
+    let mut result = &PyNone::get(values.py()).into_any().into_bound();
+    let mut max_count = 0;
+    for (value, count) in &counts {
+        if *count > max_count {
+            max_count = *count;
+            result = value.pyobject.as_any();
+        }
+    }
+    Ok(result.clone())
+}
+
+#[pymodule]
+fn frequent_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(most_frequent_naive, m)?)
+}
